c3fda2a6be93f6ab005a8fcd77ed74eb262ad839
Charlie Gooderham <charlie@euphoriq.co.uk>
2021-04-01 20:27:08 +0100

    Implement useRef to store reference to current input value. Run filteredIngredientsHandler function only 500 seconds after the user has finished typing in the filter input.

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,35 +6,40 @@ import './Search.css';
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef();
 
   useEffect(() => {
-    const queryParams = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
-
-    fetch('https://rcg-react-hooks-ed0e9-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json' + queryParams).then(
-      response => response.json()
-    ).then(
-      responseData => {
-        const loadedIngredients = [];
-
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
-          });
-        }
-
-        onLoadIngredients(loadedIngredients);
+    setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const queryParams = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+  
+        fetch('https://rcg-react-hooks-ed0e9-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json' + queryParams).then(
+          response => response.json()
+        ).then(
+          responseData => {
+            const loadedIngredients = [];
+    
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount
+              });
+            }
+    
+            onLoadIngredients(loadedIngredients);
+          }
+        );
       }
-    );
-  }, [enteredFilter, onLoadIngredients]);
+    }, 500)
+  }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={enteredFilter} onChange={e => setEnteredFilter(e.target.value)} />
+          <input type="text" ref={inputRef} value={enteredFilter} onChange={e => setEnteredFilter(e.target.value)} />
         </div>
       </Card>
     </section>
