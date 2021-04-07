faa8f1c183fff219bf16be0e3cde991a421d8891
Charlie Gooderham <charlie@euphoriq.co.uk>
2021-04-07 22:33:48 +0100

    Add custom http hook to handle HTTP requests and reduce repeating the same logic.

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,106 +1,77 @@
-import React, { useReducer, useCallback, useMemo } from 'react';
+import React, { useReducer, useEffect, useCallback, useMemo } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
+import useFetch from '../../hooks/http';
 
 const ingredientReducer = (currentIngredients, action) => {
   switch (action.type) {
-    case 'SET_INGREDIENTS':
+    case 'SET':
       return action.ingredients;
-    case 'ADD_INGREDIENT':
+    case 'ADD':
       return [...currentIngredients, action.ingredient];
-    case 'REMOVE_INGREDIENT':
+    case 'REMOVE':
       return currentIngredients.filter(ing => ing.id !== action.id);
     default:
         throw new Error('We\'ll never reach this case!');
   }
 }
 
-const httpReducer = (httpState, action) => {
-  switch (action.type) {
-    case 'SEND':
-      return { ...httpState, loading: true };
-    case 'RESPONSE':
-      return { ...httpState, loading: false }
-    case 'ERROR':
-      return { loading: false, error: action.errorMessage }
-    case 'CLEAR_ERROR':
-      return { ...httpState, error: null }
-    default:
-      throw new Error('Should not be reached!');
-  }
-}
-
 const Ingredients = () => {
   const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
-  const [httpState, dispatchHttp] = useReducer(httpReducer, { loading: false, error: null });
+  const { isLoading, error, data, sendRequest, reqExtra, reqIdentifier } = useFetch();
 
-  const addIngredientHandler = useCallback((ingredient) => {
-    dispatchHttp({ type: 'SEND' });
-    
-    fetch('https://rcg-react-hooks-ed0e9-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json', {
-      method: 'POST',
-      body: JSON.stringify(ingredient),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(response => {
-      dispatchHttp({ type: 'RESPONSE' });
-
-      return response.json();
-    }).then(responseData => {
-      dispatch({
-        type: 'ADD_INGREDIENT',
-        ingredient: {
-          id: responseData.name,
-          ...ingredient
-        }
-      })
-    }).catch(error => {
-      dispatchHttp({ type: 'ERROR', errorData: error.message });
-    });
-  }, []);
-
-  const removeIngredientHandler = useCallback((ingredientId) => {
-    dispatchHttp({ type: 'SEND' });
-
-    fetch(`https://rcg-react-hooks-ed0e9-default-rtdb.europe-west1.firebasedatabase.app/ingredients/${ingredientId}.json`, {
-      method: 'DELETE'
-    }).then(response => {
-      dispatchHttp({ type: 'RESPONSE' });
-
-      dispatch({
-        type: 'REMOVE_INGREDIENT',
-        id: ingredientId
-      });
-    }).catch(error => {
-      dispatchHttp({ type: 'ERROR', errorMessage: error.message });
-    });
-  }, []);
+  useEffect(() => {
+    if (!isLoading && !error && reqIdentifier === 'REMOVE_INGREDIENT') {
+      dispatch({ type: 'REMOVE', id: reqExtra });
+    } else if (!isLoading && !error && reqIdentifier === 'ADD_INGREDIENT') {
+      dispatch({ type: 'ADD', ingredient: {
+        id: data.name,
+        ...reqExtra
+      } });
+    }
+  }, [data, reqExtra, reqIdentifier, isLoading, error]);
 
   const filteredIngredientsHandler = useCallback(filteredIngredients => {
-    dispatch({
-      type: 'SET_INGREDIENTS',
-      ingredients: filteredIngredients
-    })
+    dispatch({ type: 'SET', ingredients: filteredIngredients });
   }, []);
 
+  const addIngredientHandler = useCallback((ingredient) => {
+    sendRequest(
+      'https://rcg-react-hooks-ed0e9-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json',
+      'POST',
+      JSON.stringify(ingredient),
+      ingredient,
+      'ADD_INGREDIENT'
+    )
+  }, [sendRequest]);
+
+  const removeIngredientHandler = useCallback((ingredientId) => {
+    sendRequest(
+      `https://rcg-react-hooks-ed0e9-default-rtdb.europe-west1.firebasedatabase.app/ingredients/${ingredientId}.json`, 
+      'DELETE',
+      null,
+      ingredientId,
+      'REMOVE_INGREDIENT'
+    );
+  }, [sendRequest]);
+
   const onCloseErrorModalHandler = useCallback(() => {
-    dispatchHttp({ type: 'CLEAR_ERROR' });
+    // dispatchHttp({ type: 'CLEAR_ERROR' });
   });
 
   const ingredientList = useMemo(() => {
     return (
-    <IngredientList ingredients={userIngredients} 
-      onRemoveItem={removeIngredientHandler} 
-      loading={httpState.loading}></IngredientList>
+    <IngredientList 
+      ingredients={userIngredients} 
+      onRemoveItem={removeIngredientHandler}></IngredientList>
   )}, [userIngredients, removeIngredientHandler]);
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} loading={httpState.loading}/>
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
@@ -108,7 +79,7 @@ const Ingredients = () => {
         { ingredientList }
       </section>
 
-      {httpState.error && <ErrorModal onClose={onCloseErrorModalHandler}>{ httpState.error }</ErrorModal>}
+      {error && <ErrorModal onClose={onCloseErrorModalHandler}>{ error }</ErrorModal>}
     </div>
   );
 }
